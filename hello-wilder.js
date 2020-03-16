1f20f747119e7e514f8e6556d683b324c54c12c8
helder-hanka <hfernandes238@gmail.com>
2020-03-16 12:52:14 +0100

    add index.html and hello-wilder.js

---
+++ hello-wilder.js
@@ -0,0 +1,5 @@
+const nameMovie = "King Kong"
+const releaseDate = 2005
+const nameDirector = "Peter Jackson"
+const message = nameMovie + " is a giant movie monster, directory by " + nameDirector + " exit in " + releaseDate;
+alert(message);
